4afd6cd32fe3928dfb91aade971df9d19d7827f0
Xinginn <46302636+Xinginn@users.noreply.github.com>
2023-02-12 21:01:38 +0100

    feat: fix slope movement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -6,16 +6,17 @@ const GRAVITY_ACCELERATION = 9.8
 const JUMP_FORCE: float = 3.5
 const MAX_JUMP: int = 2
 
-var remaining_jump = MAX_JUMP
-
 onready var look_pivot: Spatial = $LookPivot
+
+var remaining_jump = MAX_JUMP
 
 var input_move: Vector3 = Vector3()
 var gravity_local: Vector3 = Vector3()
+var snap_vector: Vector3 = Vector3()
 
 func _ready():
   Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-  
+   
 func _input(event):
   if event is InputEventMouseMotion:
     rotate_y(deg2rad(-1 * event.relative.x) * MOUSE_SENSITIVITY)
@@ -25,14 +26,20 @@ func _input(event):
 func _physics_process(delta):
   input_move = get_input_direction() * MOVE_SPEED
   if not is_on_floor():
-    gravity_local += GRAVITY_ACCELERATION * Vector3.DOWN * delta
+    gravity_local  += GRAVITY_ACCELERATION * Vector3.DOWN * delta
   else:
     remaining_jump = MAX_JUMP
-    gravity_local = GRAVITY_ACCELERATION * -get_floor_normal() * delta # preevent sliding when on ramps
+    gravity_local = Vector3.ZERO
+    
+  snap_vector = Vector3.DOWN
+  if is_on_floor():
+    snap_vector = -get_floor_normal()
+  
   if Input.is_action_just_pressed("jump") && remaining_jump > 0:
     remaining_jump -= 1
+    snap_vector = Vector3.ZERO
     gravity_local = JUMP_FORCE * Vector3.UP
-  move_and_slide(input_move + gravity_local, Vector3.UP)
+  move_and_slide_with_snap(input_move + gravity_local, snap_vector, Vector3.UP)
   
   
   
