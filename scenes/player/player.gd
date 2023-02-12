5d8a6f167e8af17e3ace973314bab14bd2ad292d
felix.ravella <felix.ravella@gmail.com>
2023-02-12 19:51:56 +0100

    initial commit

---
+++ scenes/player/player.gd
@@ -0,0 +1,42 @@
+extends KinematicBody
+
+const MOUSE_SENSITIVITY: float = 0.2
+const MOVE_SPEED: float = 3.0
+const GRAVITY_ACCELERATION = 9.8
+const JUMP_FORCE: float = 3.5
+const MAX_JUMP: int = 2
+
+var remaining_jump = MAX_JUMP
+
+onready var look_pivot: Spatial = $LookPivot
+
+var input_move: Vector3 = Vector3()
+var gravity_local: Vector3 = Vector3()
+
+func _ready():
+  Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+  
+func _input(event):
+  if event is InputEventMouseMotion:
+    rotate_y(deg2rad(-1 * event.relative.x) * MOUSE_SENSITIVITY)
+    look_pivot.rotate_x(deg2rad(event.relative.y ) * MOUSE_SENSITIVITY)
+    look_pivot.rotation.x = clamp(look_pivot.rotation.x, -1.5708, 1.5708) # clamps pivot vertical rotation between -90 and +90 °
+
+func _physics_process(delta):
+  input_move = get_input_direction() * MOVE_SPEED
+  if not is_on_floor():
+    gravity_local += GRAVITY_ACCELERATION * Vector3.DOWN * delta
+  else:
+    remaining_jump = MAX_JUMP
+    gravity_local = GRAVITY_ACCELERATION * -get_floor_normal() * delta # preevent sliding when on ramps
+  if Input.is_action_just_pressed("jump") && remaining_jump > 0:
+    remaining_jump -= 1
+    gravity_local = JUMP_FORCE * Vector3.UP
+  move_and_slide(input_move + gravity_local, Vector3.UP)
+  
+  
+  
+func get_input_direction() -> Vector3:
+  var z: float = Input.get_action_strength("forward") - Input.get_action_strength("backward")
+  var x: float = Input.get_action_strength("left") - Input.get_action_strength("right")
+  return transform.basis.xform(Vector3(x, 0, z).normalized())  # basis.xform converts local orientation to global orientation
